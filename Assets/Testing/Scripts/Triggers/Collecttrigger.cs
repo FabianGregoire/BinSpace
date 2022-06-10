db2e620fc7c74e036888ce58f2feebbe3213eb55
SamaeLight <24samael@gmail.com>
2022-06-10 09:56:29 -0500

    Compteur

--- Assets/Testing/Scripts/Triggers/Collecttrigger.cs
+++ Assets/Testing/Scripts/Triggers/Collecttrigger.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class Collecttrigger : MonoBehaviour
 {
     public AudioSource collectSound;
-    private int scoreCounter = 0;
+    public int scoreCounter = 0;
+    public TMPro.TMP_Text texte;
     // Start is called before the first frame update
     void Start()
     {
-
+        UpdateScoreText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,7 +25,12 @@ public class Collecttrigger : MonoBehaviour
             Destroy(collision.gameObject);
             collectSound.Play();
             scoreCounter++;
+            UpdateScoreText();
             Debug.Log(scoreCounter);
         }
+    }
+
+    public void UpdateScoreText(){
+        texte.text = scoreCounter.ToString();
     }
 }
